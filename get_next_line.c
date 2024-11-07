cae13fcde36ff9c7c1eff11505f20ecb9ea7ef7f
Matt7ew <123404854+Matt7ew@users.noreply.github.com>
2024-11-07 17:41:10 +0100

    made files and first functions

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,50 @@
 /*   By: matthewjorge <matthewjorge@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 15:10:16 by matthewjorg       #+#    #+#             */
-/*   Updated: 2024/11/05 15:10:17 by matthewjorg      ###   ########.fr       */
+/*   Updated: 2024/11/07 17:32:11 by matthewjorg      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+/// @brief Reads data from a file descriptor.
+/// @param fd The file descriptor to read from.
+/// @param buf The buffer to store the read data.
+/// @param count The number of bytes to read.
+/// @return The number of bytes read on success, or -1 on error.
+static ssize_t read(int fd, void *buf, size_t count)
+{
+	ssize_t ret;
+	ret = read(fd, buf, count);
+	return (ret);
+}
+int check_newline(char *line)
+{
+	int i;
+	i = 0;
+	while (line[i])
+	{
+		if (line[i] == '\n')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+char *get_next_line(int fd)
+{
+	static int count;
+	static char *line;
+	line = NULL;
+	line = (char *)malloc(BUFFER_SIZE = 1);
+	if (!line)
+		return (NULL);
+	read(fd, line, BUFFER_SIZE);
+	count += BUFFER_SIZE;
+	if (check_newline(line))
+	{
+		return (line);
+		free(line);
+		
+	}
+}
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,14 @@
 /*   By: matthewjorge <matthewjorge@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 15:10:19 by matthewjorg       #+#    #+#             */
-/*   Updated: 2024/11/05 15:10:20 by matthewjorg      ###   ########.fr       */
+/*   Updated: 2024/11/07 16:25:10 by matthewjorg      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+
+#endif
